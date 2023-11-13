7387b4fb8b1a723eaae806ba1d9200129d14f5d0
Wesley Webb <wesleywebb35@gmail.com>
2023-11-13 10:39:45 +0000

    constituencies

---
+++ app/Http/Controllers/ConstituencyController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ConstituencyResource;
+use App\Models\Constituency;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
+use Throwable;
+
+class ConstituencyController extends Controller
+{
+    public function create(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'unique:constituencies'
+            ]
+        ]);
+
+        try
+        {
+            DB::beginTransaction();
+
+            $constituency = Constituency::create($fields);
+
+            $constituency->refresh();
+            $constituency->load('region');
+
+            $constituency = new ConstituencyResource($constituency);
+
+            DB::commit();
+
+            return response()->json(compact($constituency));
+        }
+        catch(Throwable $th)
+        {
+            DB::rollBack();
+
+            throw $th;
+        }
+    }
+
+    public function index(Request $request)
+    {
+        $constituencies = ConstituencyResource::collection(Constituency::with('region')->get());
+
+        return response()->json(compact($constituencies));
+    }
+
+    public function get(Request $request, Constituency $constituency)
+    {
+        $constituency->load('region');
+
+        $constituency = new ConstituencyResource($constituency);
+
+        return response()->json(compact($constituency));
+    }
+
+    public function update(Request $request, Constituency $constituency)
+    {
+        $fields = $request->validate([
+            'name' => [
+                'required',
+                'string',
+                Rule::unique('constituencies')->ignore($constituency)
+            ]
+        ]);
+
+        try
+        {
+            DB::beginTransaction();
+
+            $constituency->update($fields);
+            $constituency->load('region');
+
+            $constituency = new ConstituencyResource($constituency);
+
+            DB::commit();
+
+            return response()->json(compact($constituency));
+        }
+        catch(Throwable $th)
+        {
+            DB::rollBack();
+
+            throw $th;
+        }
+    }
+
+    public function delete(Request $request, Constituency $constituency)
+    {
+        if($constituency->aspirants()->exists())
+        {
+            throw new NotFoundResourceException();
+        }
+
+        try
+        {
+            DB::beginTransaction();
+
+            $constituency->load('region');
+            $constituency->delete();
+
+            $constituency = new ConstituencyResource($constituency);
+
+            DB::commit();
+
+            return response()->json(compact($constituency));
+        }
+        catch(Throwable $th)
+        {
+            DB::rollBack();
+
+            throw $th;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\AspirantController;
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\ConstituencyController;
+use App\Http\Controllers\PartyController;
+use App\Http\Controllers\PositionController;
+use App\Http\Controllers\RegionController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,12 +21,114 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(AuthenticationController::class)->prefix('authentication')->name('authentication.')->group(function() {
-    Route::post('login', 'login')->name('login');
-    Route::post('register', 'register')->name('register');
-    
-    Route::middleware('auth:sanctum')->group(function() {
-        Route::post('logout', 'logout')->name('logout');
-        Route::get('user', 'user')->name('user');
-    });
-});
+Route::controller(AuthenticationController::class)->prefix('authentication')->name('authentication.')->group(
+    function() {
+        Route::post('login', 'login')->name('login');
+        Route::post('register', 'register')->name('register');
+        
+        Route::middleware('auth:sanctum')->group(
+            function() {
+                Route::post('logout', 'logout')->name('logout');
+                Route::get('user', 'user')->name('user');
+            }
+        );
+    }
+);
+
+Route::controller(RoleController::class)->prefix('roles')->name('roles.')->group(
+    function() {
+        Route::get('', 'index')->name('index');
+        Route::get('{role}', 'get')->name('get');
+    }
+);
+
+Route::controller(RegionController::class)->prefix('regions')->name('regions.')->group(
+    function() {
+        Route::get('', 'index')->name('index');
+        Route::get('{region}', 'get')->name('get');
+
+        Route::middleware('role:Administrator')->group(
+            function() {
+                Route::post('', 'create')->name('create');
+                Route::put('{region}', 'update')->name('update');
+                Route::delete('{region}', 'delete')->name('delete');
+            }
+        );
+    }
+);
+
+Route::controller(ConstituencyController::class)->prefix('constituencies')->name('constituencies.')->group(
+    function() {
+        Route::get('', 'index')->name('index');
+        Route::get('{constituency}', 'get')->name('get');
+
+        Route::middleware('role:Administrator')->group(
+            function() {
+                Route::post('', 'create')->name('create');
+                Route::put('{constituency}', 'update')->name('update');
+                Route::delete('{constituency}', 'delete')->name('delete');
+            }
+        );
+    }
+);
+
+Route::controller(PositionController::class)->prefix('positions')->name('positions.')->group(
+    function() {
+        Route::get('', 'index')->name('index');
+        Route::get('{position}', 'get')->name('get');
+
+        Route::middleware('role:Administrator')->group(
+            function() {
+                Route::post('', 'create')->name('create');
+                Route::put('{position}', 'update')->name('update');
+                Route::delete('{position}', 'delete')->name('delete');
+            }
+        );
+    }
+);
+
+Route::controller(PartyController::class)->prefix('parties')->name('parties.')->group(
+    function() {
+        Route::get('', 'index')->name('index');
+        Route::get('{party}', 'get')->name('get');
+
+        Route::middleware('role:Administrator')->group(
+            function() {
+                Route::post('', 'create')->name('create');
+                Route::put('{party}', 'update')->name('update');
+                Route::delete('{party}', 'delete')->name('delete');
+            }
+        );
+    }
+);
+
+Route::controller(AspirantController::class)->prefix('aspirants')->name('aspirants.')->group(
+    function() {
+        Route::middleware('role:Administrator')->group(
+            function() {
+                Route::prefix('creation-requests')->group(
+                    function() {
+                        Route::get('', 'indexCreationRequest')->name('indexCreationRequest');
+                        Route::get('{aspirant_creation_request}', 'getCreationRequest')->name('getCreationRequest');
+                    }
+                );
+
+                Route::prefix('update-requests')->group(
+                    function() {
+                        Route::get('', 'indexUpdateRequest')->name('indexUpdateRequest');
+                        Route::get('{aspirant_update_request}', 'getUpdateRequest')->name('getUpdateRequest');
+                    }
+                );
+
+                Route::delete('{aspirant}', 'delete')->name('delete');
+            }
+        );
+
+        Route::put('{aspirant}', 'update')->middleware('role:Aspirant')->name('update');
+        
+        Route::post('', 'create')->middleware('role:Follower')->name('create');
+
+        Route::get('', 'index')->name('index');
+        Route::get('{aspirant}', 'get')->name('get');
+    }
+);
